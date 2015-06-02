f0bad19da63df511d416fabaa665bb8fe1e62949
Joan Saez <janisaez@gmail.com>
2015-06-02 13:18:26 +0100

    initial commit

--- app/models/activitat.rb
+++ app/models/activitat.rb
@@ -4,12 +4,18 @@ class Activitat < ActiveRecord::Base
   has_attached_file :cover,
                     styles: {
                         thumb: '100x100>',
-                        square: '200x200',
-                        medium: '300x300>'
+                        square: '200x200>',
+                        medium: '300x300>',
+                        cover: '640x480>'
                     },
                     :s3_host_name => 's3-eu-west-1.amazonaws.com'
 
+
+  validates_attachment :cover, :presence => true
   validates_attachment_content_type :cover, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_file_name :cover, :matches => [/png\Z/, /jpe?g\Z/]
+  validates_attachment_size :cover,  :less_than => 10.megabytes
+
 
   def full_path
     "#{cover.url(:medium)}"
