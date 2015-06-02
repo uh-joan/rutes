103f52a18e5d226d69268196d3ea8acc9303f804
Joan Saez <janisaez@gmail.com>
2015-06-02 14:27:10 +0100

    initial commit

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Rutes
         resource '*', :headers => :any, :methods => [:get, :post, :delete, :put, :patch, :options], :max_age => 0
       end
     end
-    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
